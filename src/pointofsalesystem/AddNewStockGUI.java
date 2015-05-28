564f669c8e7bd17745213d2eba65010c717efdfe
Daniel Botha <botha.daniel1@gmail.com>
2015-05-28 21:43:20 +0200

    egyption brackets....{}{}{}{}

--- src/pointofsalesystem/AddNewStockGUI.java
+++ src/pointofsalesystem/AddNewStockGUI.java
@@ -68,10 +68,8 @@ public class AddNewStockGUI extends javax.swing.JFrame
         new ManagerPortalGUI().setVisible(true);//Bring up the manager portal
     }//GEN-LAST:event_lblBackMouseReleased
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
+    public static void main(String args[]) 
+    {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
